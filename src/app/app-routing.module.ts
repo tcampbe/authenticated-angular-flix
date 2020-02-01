e4869c0f149aaf6ebeb8ddc68f00a9efef5a3d96
tcampbe <tcampb@icloud.com>
2020-02-01 17:39:13 -0600

    routing some

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { TitleListComponent } from './title-list/title-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: MainComponent },
+  { path: 'titlelist', component: TitleListComponent },
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/list-toggle/list-toggle.component.ts
+++ src/app/list-toggle/list-toggle.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit,Input } from '@angular/core';
-import {MovieService} from '../movie.service'
+import { Component, OnInit, Input } from "@angular/core";
+import { MovieService } from "../movie.service";
+
 @Component({
-  selector: 'app-list-toggle',
-  templateUrl: './list-toggle.component.html',
-  styleUrls: ['./list-toggle.component.css']
+  selector: "app-list-toggle",
+  templateUrl: "./list-toggle.component.html",
+  styleUrls: ["./list-toggle.component.css"]
 })
 export class ListToggleComponent implements OnInit {
   @Input() movie;
-  constructor(private movieService : MovieService) { }
+  constructor(private movieService: MovieService) {}
 
-  ngOnInit() {
-  }
-  saveMovieToList(){
+  ngOnInit() {}
+  saveMovieToList() {
     this.movieService.saveToList(this.movie);
   }
-
 }
